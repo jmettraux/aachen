0aedc3d9d86c29050ecf9e27250a31cd7c1e11cc
John Mettraux <jmettraux@gmail.com>
2021-09-17 13:35:20 +0900

    Gather 6x35 names

---
+++ tmp/names.rb
@@ -0,0 +1,80 @@
+
+# :map q :.w >> tmp/names.txt<ENTER>
+
+
+frankish_male_names = %w{
+  Adalbert(us)
+  Agobard
+  Audomar
+  Bern(h)ard(us)
+  Burchard
+  Carloman
+  Chilperic
+  Dagobert
+  Drogo(n)
+  Eberhard
+  Ebrulf
+  Fredegar
+  Fridolin
+  Fulk
+  Ger(a,o)ld
+  Goscelin
+  Hartgard
+  Hilduin
+  Ingomer
+  Jocelin
+  Karlmann
+  Leu(t)fr(e,i)d
+  Martin(us)
+  Nor(d)bert
+  Odo
+  Ouen
+  Pancras
+  Philibert
+  Reginald
+  Sigismund
+  Theodemir
+  Unroch
+  Vigor
+  Walaric
+  Zwentibold
+}
+
+frankish_female_names = %w{
+  Agatha
+  Ber(g,i)undis
+  Clothild
+  Danburga
+  Engelberga
+  Ermina
+  Fara
+  Fredegunde
+  Geneva
+  Gis(e)la
+  Helinda
+  Herenfrida
+  Hruodgarda
+  Imma
+  Irmengard
+  Joveta
+  Leuekin
+  Liedrada
+  Merofled
+  Merwig
+  Nidlebis
+  Nordrada
+  Ogiva
+  Olga
+  Plectrudis
+  Radeken
+  Regana
+  Rosamund
+  Sig(,e)berta
+  Susanna
+  Teutberga
+  Theodelinda
+  Ultrogotha
+  Vulfegundis
+  Wisigard
+}
+
