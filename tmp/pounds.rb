b157edeb05363ff19b7f5ec62a587e9c7e986e2c
John Mettraux <jmettraux@gmail.com>
2022-03-12 11:56:56 +0900

    Enhance lb to kg scale, show DC *4 and *6

--- tmp/pounds.rb
+++ tmp/pounds.rb
@@ -1,14 +1,16 @@
 
 start = 10
-inc = 5
-max = 250
+inc = 4
+max = 242
 ptk = 0.453592
 
-puts '  | lb  | kg    |'
-puts '  |----:|------:|'
+puts '  | *4 | lb  | kg    | *6 |'
+puts '  |---:|----:|------:|---:|'
   #
 loop do
-  puts "  | %3d | %#5.1f |" % [ start, start * ptk ]
+  s4 = start / 4; s4 = (s4 < 3 || s4 > 18) ? '  ' : '%2d' % s4
+  s6 = start / 6; s6 = (s6 < 3 || s6 > 18) ? '  ' : '%2d' % s6
+  puts "  | %s | %3d | %#5.1f | %s |" % [ s4, start, start * ptk, s6 ]
   start += inc
   break if start > max
 end
